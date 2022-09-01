c36132dc83146035367f25b15ad6f01e4f23bea1
Mathieu Ibersien <mat98-76@live.fr>
2022-09-01 22:50:27 +0200

    [TEST] Repository tests of H2 and MySQL db fixed

--- backend/src/test/java/com/example/starshipShop/manufacturer/ManufacturerRepositoryH2Test.java
+++ backend/src/test/java/com/example/starshipShop/manufacturer/ManufacturerRepositoryH2Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -17,62 +17,49 @@ import com.example.starshipShop.jpa.Manufacturer;
 import com.example.starshipShop.repository.ManufacturerRepository;
 
 /**
- * @author Mathieu
- *
- *         Test some repository operations on an H2 in memory Database. Same
- *         test class as {@link ManufacturerRepositoryMysqlTest}
- */
+* @author Skrollu
+*
+*         Test some repository operations on an H2 in memory Database. Same
+*         test class as {@link ManufacturerRepositoryMysqlTest}
+*/
 @DataJpaTest
 @ActiveProfiles("test-h2")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DisplayName("Simple Manufacturer repository with an H2 in memory database")
 public class ManufacturerRepositoryH2Test {
 
 	@Autowired
-	private TestEntityManager entityManager;
-
-	@Autowired
 	private ManufacturerRepository manufacturerRepository;
-
-/* 	@Test
-	void shouldGetManufacturersFromDb() throws Exception {
-		List<Manufacturer> manufacturers = manufacturerRepository.findAll();
-		assertEquals(2, manufacturers.size());
-	}
-
+	
 	@Test
+	@DisplayName("Find all manufacturers and check the size of the list")
+	void shouldGetAllManufacturersFromDb() throws Exception {
+		List<Manufacturer> manufacturers = manufacturerRepository.findAll();
+		assertEquals(6, manufacturers.size());
+	}
+	
+	@Test
+	@DisplayName("Get a manufacturer by id")
 	void shouldGetManufacturerByIdFromDb() throws Exception {
-		final String expectedName = "Koensayr Manufacturing";
+		final String expectedName = "TestManufacturer1";
 		Manufacturer manufacturer = manufacturerRepository.getById(1L);
 		assertNotNull(manufacturer);
 		assertEquals(manufacturer.getId(), 1L);
 		assertEquals(manufacturer.getName(), expectedName);
 	}
-
+	
 	@Test
+	@DisplayName("Find all manufacturers and retrieve one by name in the obtained list")
 	void shouldGetManufacturerByNameFromDb() throws Exception {
 		List<Manufacturer> manufacturers = manufacturerRepository.findAll();
-		final String expected = "Koensayr Manufacturing";
+		final String expected = "TestManufacturer1";
 		Manufacturer manufacturer = manufacturers	.stream()
-													.filter(m -> m	.getName()
-																	.equals(expected))
-													.findFirst()
-													.get();
+		.filter(m -> m	.getName()
+		.equals(expected))
+		.findFirst()
+		.get();
 		assertNotNull(manufacturer);
-		assertNotEquals(manufacturer.getName(), expected + "g");
+		assertNotEquals(manufacturer.getName(), expected + "wrong");
 		assertEquals(manufacturer.getName(), expected);
 	}
-
-	@Test
-	void shouldSaveNewManufacturerAndGetItByNameFromDb() throws Exception {
-		final String manufacturerName = "Corellian Engineering Corporation";
-		Manufacturer manufacturer = new Manufacturer();
-		manufacturer.setName(manufacturerName);
-		this.entityManager.persist(manufacturer);
-
-		Manufacturer result = this.manufacturerRepository	.findByName(manufacturerName)
-															.get();
-		assertNotNull(result);
-		assertEquals(result.getId(), 3L);
-		assertEquals(result.getName(), manufacturerName);
-	} */
 }
