2df93a9e484cb94de55281550a7c9fd14b60f50e
Mathieu Ibersien <mat98-76@live.fr>
2023-02-26 21:34:02 +0100

    fix(gateway): replace zuul by starter gateway

--- gateway-server/src/main/java/com/starshipshop/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/starshipshop/gatewayserver/GatewayServerApplication.java
@@ -1,16 +1,10 @@
-package com.starshipshop.discoveryserver;
+package com.starshipshop.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class GatewayServerApplication {
 
