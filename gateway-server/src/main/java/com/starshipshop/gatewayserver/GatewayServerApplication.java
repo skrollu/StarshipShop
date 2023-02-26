89b5df18b70b5de22e44f19fef84f8da70b8c507
Mathieu Ibersien <mat98-76@live.fr>
2023-02-26 16:05:37 +0100

    fix(gateway/wip): add a gateway

---
+++ gateway-server/src/main/java/com/starshipshop/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,21 @@
+package com.starshipshop.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+}
