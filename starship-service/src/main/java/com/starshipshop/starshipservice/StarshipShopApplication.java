d8874b6d9d1278932eff28037496db0c19e95a41
Mathieu Ibersien <mat98-76@live.fr>
2023-03-05 12:17:36 +0100

    feat(starship): cloudify starship service
    config is now held in cloud and support dev profile
    connect service to eureka server

--- starship-service/src/main/java/com/starshipshop/starshipservice/StarshipShopApplication.java
+++ starship-service/src/main/java/com/starshipshop/starshipservice/StarshipShopApplication.java
@@ -2,20 +2,13 @@ package com.starshipshop.starshipservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import lombok.extern.slf4j.Slf4j;
-
 @SpringBootApplication
-@Slf4j
+@EnableDiscoveryClient
 @EnableJpaRepositories(enableDefaultTransactions = false)
 public class StarshipShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StarshipShopApplication.class, args);
-		log.warn("Application started with the following arguments:");
-		for (String s : args) {
-			log.warn("=> " + s);
-		}
-	}
+	public static void main(String[] args) { SpringApplication.run(StarshipShopApplication.class, args); }
 }
