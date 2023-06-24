0500be625151bd19960f7ba73f9a78d9292caf59
Mathieu Ibersien <mat98-76@live.fr>
2023-06-24 15:53:21 +0200

    fix(ci): add an empty test for image service

---
+++ image-service/src/test/java/com/starshipshop/imageservice/ImageServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.starshipshop.imageservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ImageServiceApplicationTest {
+
+    @Disabled
+    void test() {
+
+    }
+}
